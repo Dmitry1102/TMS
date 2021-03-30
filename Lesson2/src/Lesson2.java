ff3bae82ec460c7ec0c44cb0fba6c42e87593c30
Dmitry1102 <meleshkevich2016@mail.ru>
2021-03-30 15:35:41 +0200

    First Practise

--- Lesson2/src/Lesson2.java
+++ Lesson2/src/Lesson2.java
@@ -1,7 +1,9 @@
 public class Lesson2 {
     public static void main(String[] args) {
+        System.out.println("Fisrt:");
         System.out.println(sum(100, 200));
         //System.out.println(sum(Integer.MAX_VALUE, Integer.MAX_VALUE));
+
         System.out.println(max(56, 349));
         System.out.println(average(new int[]{0, -2, 3, -1, 5}));
         System.out.println(max(new int[]{1, 2, 3, 4, 5, 100, 99}));
