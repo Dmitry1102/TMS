32a2d903c2d4d178821629ec3245c72f2ba500d9
Dmitry1102 <meleshkevich2016@mail.ru>
2021-04-10 21:06:53 +0200

    practise 5

--- Lesson5/src/com/home/model/Computer.java
+++ Lesson5/src/com/home/model/Computer.java
@@ -1,5 +1,15 @@
 package com.home.model;
 
-public class Computer {
+public class Computer  {
+
+    String processor;
+    String operativeMemory;
+    String hardDrive;
+    Boolean resourceActivity;
+
+    
+
+
+
 
 }
