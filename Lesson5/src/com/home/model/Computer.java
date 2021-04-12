9ae5ab4b9200b515c11e853ba967dfa6812a2362
Dmitry1102 <meleshkevich2016@mail.ru>
2021-04-12 10:58:19 +0200

    Changed practise 5

--- Lesson5/src/com/home/model/Computer.java
+++ Lesson5/src/com/home/model/Computer.java
@@ -1,15 +1,60 @@
 package com.home.model;
 
-public class Computer  {
+import java.util.Random;
+import java.util.Scanner;
+
+public class Computer {
 
     String processor;
     String operativeMemory;
     String hardDrive;
-    Boolean resourceActivity;
+    int resourceActivity;
 
-    
+    public Computer(String s, String s1, String s2, int i) {
+        processor = s;
+        operativeMemory = s1;
+        hardDrive = s2;
+        resourceActivity = i;
+    }
+
+    public void printResult(){
+        System.out.println("processor: " + processor );
+        System.out.println("operative memory: " + operativeMemory );
+        System.out.println("hard drive disk: " + hardDrive );
+        System.out.println("resource activity : " + resourceActivity );
+
+    }
 
 
+    public void on() {
+        try {
+            Random random = new Random();
+            Scanner scanner = new Scanner(System.in);
+            int myNumber = scanner.nextInt() ;
+            int number;
+            do {
+                number = random.nextInt();
+            } while (resourceActivity < number);
+            error(number);
+            if (number == myNumber) {
+                off();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Внимание! Введите 0 или 1");
+        }
+
+    }
 
 
+    private void off() {
+
+    }
+
+
+    private void error(int a) {
+        if (a > resourceActivity) {
+            System.out.println("Компьютер сгорел!");
+        }
+    }
 }
--- Lesson5/src/com/home/service/Main.java
+++ Lesson5/src/com/home/service/Main.java
@@ -1,8 +1,14 @@
 package com.home.service;
 
+import com.home.model.Computer;
+
+
+
 public class Main {
     public static void main(String[] args) {
-
+        Computer computer = new Computer("Intel I7","RAM23","HDD211", 5);
+        computer.printResult();
+        computer.on();
     }
 
 }
