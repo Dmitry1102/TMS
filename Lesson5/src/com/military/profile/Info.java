dcbb04b053fa54f86b0cf615d1252c644407bf0d
Dmitry1102 <meleshkevich2016@mail.ru>
2021-04-17 01:47:16 +0200

    Practice 6

--- Lesson5/src/com/military/profile/Info.java
+++ Lesson5/src/com/military/profile/Info.java
@@ -1,54 +1,79 @@
 package com.military.profile;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Info {
-    private final Scanner scanner = new Scanner(System.in);
+    private static final Scanner in = new Scanner(System.in);
+    private static final PersonRegistry personRegistry = new PersonRegistry(new ArrayList<>());
+    public final String CITY = "Минск";
+    public String NAME = "Александр";
+    public final String MAN = "Мужчина";
+    public final String COUNTRY = "Беларусь";
 
-    private int getEnterPersonNumbers() {
-        System.out.println("Введите количество: ");
-        return Integer.parseInt(scanner.nextLine());
-    }
+    private void enterPerson() {
+        System.out.println("Enter name: ");
+        String name;
+        while (!(name = in.nextLine()).matches("[а-яА-Я]+")) ;
+        System.out.println("Enter sex: ");
+        String sex;
+        while (!(sex = in.nextLine()).matches("[а-яА-Я]+")) ;
+        System.out.println("Enter age: ");
+        int age = in.nextInt();
+        System.out.println("Enter city: ");
+        String city;
+        while (!(city = in.nextLine()).matches("[а-яА-Я]+")) ;
+        System.out.println("Enter country: ");
+        String country;
+        while (!(country = in.nextLine()).matches("[а-яА-Я]+")) ;
+        Address address = new Address(city, country);
+        Person person = new Person(name, age, sex, address);
+        if (country.equals(COUNTRY)) {
+            if (sex.equals(MAN)) {
+                if (age >= 18 && age < 27) {
+                    personRegistry.getPersonList().add(person);
+                } else {
+                    System.out.println("Не проходит до призыва по возрасту...");
+                }
+            } else {
+                System.out.println("Женшины не являются военнообязаными...");
 
-    private void enterName(Person person) {
-        System.out.println("Введите имя: ");
-        person.setName(scanner.nextLine());
-    }
-
-    private void enterAge(Person person) {
-        System.out.println("Введите возрвст: ");
-        person.setAge(Integer.parseInt(scanner.nextLine()));
-    }
-
-//    private void enterSex() {
-//        System.out.println("Введите пол: ");
-//        person.setSex(scanner.nextLine());
-//    }
-//
-//    private void enterCity() {
-//        System.out.println("Введите город: ");
-//        adress.setCity(scanner.nextLine());
-//    }
-//
-//    private void enterCountry() {
-//        System.out.println("Введите страну: ");
-//        adress.setCountry(scanner.nextLine());
-//    }
-
-    public List<Person> getPreparePersons() {
-        List<Person> personList = new ArrayList<>();
-        int personNumbers = getEnterPersonNumbers();
-        for (int i = 0; i < personNumbers; i++) {
-            Person person = new Person();
-            enterName(person);
-            enterAge(person);
-            personList.add(person);
-            //дальше доделайте логику
-//            enterCity();
-//            enterCountry();
+            }
+        } else {
+            System.out.println("Гражданин Другого Государства...");
         }
-        return personList;
+        System.out.println();
     }
 
-}
+    public void enterPersons() {
+        System.out.println("Enter count: ");
+        int count = in.nextInt();
+        for (int i = 0; i < count; i++) {
+            enterPerson();
+        }
+    }
+
+    public void rightCity() {
+        for (var person : personRegistry.getPersonList()) {
+            if (person.getAdress().getCity().equals(CITY)) {
+                System.out.println(person);
+            }
+        }
+    }
+
+    public void ageCompare() {
+        for (var person : personRegistry.getPersonList()) {
+            if (person.getAge() >= 25 && person.getAge() < 27) {
+                System.out.println(person);
+            }
+        }
+    }
+
+    public void nameCompare() {
+        for (var person : personRegistry.getPersonList()) {
+            if (person.getName().equals(NAME)) {
+                System.out.println(person);
+            }
+        }
+    }
 }
--- Lesson5/src/com/military/profile/Main.java
+++ Lesson5/src/com/military/profile/Main.java
@@ -1,96 +1,26 @@
 package com.military.profile;
 
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
 
 public class Main {
-    private static final Scanner in = new Scanner(System.in);
-    private static final PersonRegistry personRegistry = new PersonRegistry(new ArrayList<>());
-    public static final String CITY = "Минск";
-    public static final String NAME = "Александр";
-    public static final String MAN = "Мужчина";
-    public static final String COUNTRY = "Беларусь";
-
-    public static void enterPerson() {
-        System.out.println("Enter name: ");
-        String name;
-        while (!(name = in.nextLine()).matches("[а-яА-Я]+")) ;
-        System.out.println("Enter sex: ");
-        String sex;
-        while (!(sex = in.nextLine()).matches("[а-яА-Я]+")) ;
-        System.out.println("Enter age: ");
-        int age = in.nextInt();
-        System.out.println("Enter city: ");
-        String city;
-        while (!(city = in.nextLine()).matches("[а-яА-Я]+")) ;
-        System.out.println("Enter country: ");
-        String country;
-        while (!(country = in.nextLine()).matches("[а-яА-Я]+")) ;
-        Address address = new Address(city, country);
-        Person person = new Person(name, age, sex, address);
-        if (country.equals(COUNTRY)) {
-            if (sex.equals(MAN)) {
-                if (age >= 18 && age < 27) {
-                    personRegistry.getPersonList().add(person);
-                } else {
-                    System.out.println("Не проходит до призыва по возрасту...");
-
-                }
-            } else {
-                System.out.println("Женшины не являются военнообязаными...");
-
-            }
-        } else {
-            System.out.println("Гражданин Другого Государства...");
-        }
-        System.out.println();
-    }
-
-    public static void enterPersons() {
-        System.out.println("Enter count: ");
-        int count = in.nextInt();
-        for (int i = 0; i < count; i++) {
-            enterPerson();
-        }
-    }
-
-    public static void rightCity() {
-        for (var person : personRegistry.getPersonList()) {
-            if (person.getAdress().getCity().equals(CITY)) {
-                System.out.println(person);
-            }
-        }
-    }
-
-    public static void ageCompare() {
-        for (var person : personRegistry.getPersonList()) {
-            if (person.getAge() >= 25 && person.getAge() < 27) {
-                System.out.println(person);
-            }
-        }
-    }
-
-    public static void nameCompare() {
-        for (var person : personRegistry.getPersonList()) {
-            if (person.getName().equals(NAME)) {
-                System.out.println(person);
-            }
-        }
-    }
-
 
     public static void main(String[] args) {
-        enterPersons();
+        List<Person> personList = new ArrayList<>();
+        Info info = new Info();
+        info.enterPersons();
+        PersonRegistry personRegistry = new PersonRegistry(personList);
         for (var person : personRegistry.getPersonList()) {
-            System.out.println(person);
+            System.out.println(person.getName());
         }
         System.out.println("Призывники города Минска: ");
-        rightCity();
+        info.rightCity();
         System.out.println("Призывники от 25 до 27: ");
-        ageCompare();
+        info.ageCompare();
         System.out.println("Призывники c именем Александр: ");
-        nameCompare();
+        info.nameCompare();
 
     }
+
 
 }
